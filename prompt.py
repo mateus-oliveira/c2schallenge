24764d1dde124be193dc0e6a5a9309f52a974b99
Mateus Oliveira <devmateusalves@gmail.com>
2025-04-26 23:43:37 -0300

    feat: llm prompt base with OpenAI

--- prompt.py
+++ prompt.py
@@ -1,20 +1,47 @@
-import os
+import json
 import requests
 
-from dotenv import load_dotenv
 from fastapi import status
+from openai import OpenAI
 
 from app.models.enums import Transmission, Fuel
+from app.settings import BASE_URL, OPEN_AI_API_KEY
 
-load_dotenv()
 
+def llm_interpreter(sentence: str) -> dict:
+    prompt = f"""
+    Voce é um agente que ajuda a buscar veículos em um banco de dados.
+    Dado o texto abaixo, extraia os possíveis filtros e retorne em formato JSON:
 
-def llm_interpreter(message: str) -> dict:
-    # TODO: LLM integration
-    return {
-        "brand": "FIAT",
-        "model": "PUNTO",
-    }
+    {{
+        "brand": str | null,
+        "model": str | null,
+        "year": int | null,
+        "engine": str | null,
+        "fuel": str | null, // "FLEX", "GASOLINE", "HYBRID", "ELETRIC" or "DIESEL"
+        "color": str | null,
+        "mileage": int | null,
+        "doors": int | null,
+        "transmission": str | null, // "MANUAL", "AUTOMATIC" or "CVT"
+        "price": float | null,
+    }}
+
+    Texto: "{sentence}"
+    """
+    client = OpenAI(api_key=OPEN_AI_API_KEY)
+    try:
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",  # or "gpt-4" for more advanced capabilities
+            messages=[
+                {"role": "system", "content": prompt},
+                {"role": "user", "content": sentence}
+            ],
+            response_format={"type": "json_object"}
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        print(f"Error when calling Open AI: {e}")
+        return {}
 
 
 def validate_transmission(transmission: str) -> str:
@@ -35,19 +62,31 @@ def main():
     """
     Main function to interact with the user and fetch car data.
     """
-    sentence = input("Qual carro está procurando? ")
-    parameters = llm_interpreter(sentence)
-    
+    brand = input("Qual marca e modelo do carro que você está buscando? ")
+    year = input("Qual ano do carro? ")
+    price = input("Qual o preço máximo que você está disposto a pagar? ")
+    other_info = input("Mais algum detalhe para sua busca? ")
+
+    parameters = llm_interpreter(
+        f"""
+        Busque um carro com os seguintes detalhes:
+        Marca/Modelo: {brand}
+        Ano: {year}
+        Preço: {price}
+        Outros detalhes: {other_info}
+        """
+    )
+
     if 'transmission' in parameters:
         parameters['transmission'] = validate_transmission(parameters['transmission'])
     
     if 'fuel' in parameters:
         parameters['fuel'] = validate_fuel(parameters['fuel'])
     
-    response = requests.get(f'{os.getenv("BASE_URL")}/cars', params=parameters)
+    response = requests.get(f'{BASE_URL}/cars', params=parameters)
     
     if response.status_code == status.HTTP_200_OK:
-        print(response.json())
+        print(json.dumps(response.json(), indent=2, ensure_ascii=False))
     else:
         print(f"Error: {response.status_code}")
 
